b9a3fc1399923a655ade65d33008893454ccfec5
Ufuk <ufkyvz@hotmail.com>
2022-12-18 11:31:05 +0300

    Deneme_Deneme Classı ekledi

---
+++ src/ProjeAkakce/Deneme_denemeClassiEkledi.java
@@ -0,0 +1,9 @@
+package ProjeAkakce;
+
+public class Deneme_denemeClassiEkledi {
+    public static void main(String[] args) {
+
+        System.out.println("Deneme_DenemeClassiEkledi");
+
+    }
+}
